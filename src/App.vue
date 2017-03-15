2572ad5504893d6285718a1cb641cd37d1098405
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2017-03-14 23:21:19 -0300

    Fazendo a bosta da modal

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="texto">
+    <button type="button" @click="showModal = true">Abrir Modal</button>
+
+    <modal v-if="showModal" @close="showModal = false" :text="texto">Fecha essa porra</modal>
+
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      showModal: false,
+      texto: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
